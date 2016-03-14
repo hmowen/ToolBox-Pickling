18011ec69643b5771f7b1558c501ab1fd0e49ab6
hmowen <harper.owen@students.olin.edu>
2016-03-13 23:30:46 -0400

    implemented pickle code

--- counter.py
+++ counter.py
@@ -2,7 +2,10 @@
 
 from os.path import exists
 import sys
+import os
 from pickle import dump, load
+import pickle
+#from pattern.web import *
 
 def update_counter(file_name, reset=False):
 	""" Updates a counter stored in the file 'file_name'
@@ -29,7 +32,47 @@ def update_counter(file_name, reset=False):
 	>>> update_counter('blah2.txt')
 	2
 	"""
-	pass
+# ts_eliot_texts_FULL = URL('http://www.gutenberg.org/cache/epub/1567/pg1567.txt').download()
+# f = open('ts_eliot_full.pickle','w')
+# pickle.dump(ts_eliot_texts_FULL,f)
+# f.close()
+# input_file = open('ts_eliot_full.pickle','r')
+# reloaded_copy_of_eliot_texts = pickle.load(input_file)
+	if os.path.exists(file_name) == False:
+		f = open(file_name, 'w')
+		counter = 1
+		pickle.dump(counter, f)
+		f.close
+		return counter
+
+	elif os.path.exists(file_name) == True:
+		if reset == True:
+			f = open(file_name, 'w')
+			counter = 1
+			pickle.dump(counter, f)
+			f.close
+			return counter
+
+		else:
+			f = open(file_name, 'r+')
+			counter = pickle.load(f)
+			counter += 1
+			f.seek(0,0)
+			pickle.dump(counter, f)
+			f.close
+			return counter
+
+	print counter
+
+
+
+
+# # Save data to a file (will be part of your data fetching script)
+# lincoln_speeches_FULL = URL('http://www.gutenberg.org/cache/epub/14721/pg14721.txt').download()
+# f = open('lincoln_speeches.pickle','w')
+# pickle.dump(lincoln_speeches_FULL,f)
+# f.close()
+
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
